08c951420b068bd1846fb406d2e76faf02560798
Yung Cher Ho <ycho527@hotmail.com>
2020-03-11 16:37:29 +0800

    Created new and index pages

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,34 @@
+class FlatsController < ApplicationController
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create(name: "Nice central apartment in #{Faker::Address.city}",
+              address: Faker::Address.street_address,
+              description: 'Beautiful bright room',
+              price_per_night: (35..240).to_a.sample,
+              number_of_guests: (1..5).to_a.sample)
+end
